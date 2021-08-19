5f252cf1adf6c1ddfec6eece227109ab2a6bfcc4
Lambert <87885422+s25asq@users.noreply.github.com>
2021-08-19 21:36:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const panels = document.querySelectorAll('.panel')
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
